e9bd4d5015b147c21835c39a8f99bff7ed85271d
Tina <migujeong@gmail.com>
2022-08-15 20:14:00 +0900

    first commit with basic REST API

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request , Response , NextFunction} from 'express';
+import toDoRoutes from './routes/todos'
+import {json} from 'body-parser';
+
+const app = express();
+app.use(json());
+app.use('/todos', toDoRoutes);
+app.use((err : Error, req: Request, res : Response, next : NextFunction) => {
+    res.status(500).json({message : err.message + req.body.toString()});    
+});
+app.listen(3000);
---
+++ src/routes/todos.ts
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+import {getTodos, createTodos, updateTodo, deleteTodo} from '../controller/todos';
+
+const router = Router();
+router.get('/',getTodos);
+router.post('/',createTodos);
+router.patch('/:id',updateTodo);
+router.delete('/:id',deleteTodo);
+
+export default router;
