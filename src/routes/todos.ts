92b5b592c752094ad2234107871fd98483d1bbc1
tinajeong <starandpoem143@gmail.com>
2022-08-16 12:45:04 +0900

    [config] add prettier setting

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,10 +1,15 @@
-import {Router} from 'express';
-import {getTodos, createTodos, updateTodo, deleteTodo} from '../controller/todos';
+import {Router} from 'express'
+import {
+    getTodos,
+    createTodos,
+    updateTodo,
+    deleteTodo,
+} from '../controller/todos'
 
-const router = Router();
-router.get('/',getTodos);
-router.post('/',createTodos);
-router.patch('/:id',updateTodo);
-router.delete('/:id',deleteTodo);
+const router = Router()
+router.get('/', getTodos)
+router.post('/', createTodos)
+router.patch('/:id', updateTodo)
+router.delete('/:id', deleteTodo)
 
-export default router;
+export default router
